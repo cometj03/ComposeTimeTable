98eb9cfbf6c21a6357fa8a8721aef82d322f88ef
정해성 <harryjung1001@gmail.com>
2022-11-18 20:26:43 +0900

    Rename: TimeTable -> ComposeTimeTable

---
+++ ComposeTimeTable/src/main/java/com/cometj03/composetimetable/ComposeTimeTable.kt
@@ -0,0 +1,118 @@
+package com.cometj03.composetimetable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.LocalDateTime
+import java.time.LocalTime
+import kotlin.math.max
+import kotlin.math.min
+
+@Composable
+fun ComposeTimeTable(
+    dayNames: List<String>,
+    timeTableData: TimeTableData,
+    onCellClick: (ScheduleEntity) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val startHour = min(timeTableData.earliestStartHour, tableStartHour)
+    val endHour = max(timeTableData.latestEndHour, tableEndHour)
+    val hours = (startHour..endHour).toList()
+
+    val cellCountList = timeTableData.entitiesCountList
+
+    require(dayNames.size == cellCountList.size) {
+        "size of dayNames should be equal to count of scheduleEntities"
+    }
+
+    TimeTable(
+        columnCount = cellCountList.size,
+        dayHeader = {
+            DayHeader(dayNames[it])
+        },
+        hoursLabel = {
+            HoursLabel(hours)
+        },
+        cellCountList = cellCountList,
+        timeCell = { column, index ->
+            val dayData = timeTableData.scheduleDayDataList[column]
+            val scheduleEntity = dayData.scheduleEntities[index]
+
+            val beforeCellEndTime = if (index == 0) {
+                LocalDateTime.of(
+                    scheduleEntity.startTime.toLocalDate(),
+                    LocalTime.of(hours.first(), 0)
+                )
+            } else {
+                dayData.scheduleEntities[index - 1].endTime
+            }
+
+            TimeTableCell(
+                cellData = scheduleEntity,
+                onCellClick = onCellClick,
+                modifier = Modifier
+                    .timeTableCell(
+                        beforeCellEndTime,
+                        scheduleEntity.startTime,
+                        scheduleEntity.endTime,
+                        hours.size
+                    )
+            )
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun DayHeader(dayName: String) {
+    Text(
+        text = dayName,
+        style = MaterialTheme.typography.subtitle1, // TODO: TextStyle
+        modifier = Modifier
+            .width(tableColumnWidth)
+            .padding(vertical = 4.dp),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun HoursLabel(hours: List<Int>) {
+    Column {
+        hours.forEach {
+            Text(
+                text = "$it",
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier
+                    .width(40.dp)
+                    .height(60.dp)
+                    .padding(4.dp),
+                textAlign = TextAlign.End
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HoursLabelPreview() {
+    HoursLabel(hours = (9..16).toList())
+}
+
+@Preview
+@Composable
+fun TimeTablePreview() {
+    val dayNames = listOf("월", "화", "수", "목", "금")
+    ComposeTimeTable(
+        dayNames = dayNames,
+        timeTableData = timeTableData,
+        onCellClick = {}
+    )
+}
--- ComposeTimeTable/src/main/java/com/cometj03/composetimetable/TimeTable.kt
+++ ComposeTimeTable/src/main/java/com/cometj03/composetimetable/TimeTable.kt
@@ -1,118 +1,115 @@
 package com.cometj03.composetimetable
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.Layout
 import java.time.LocalDateTime
-import java.time.LocalTime
-import kotlin.math.max
-import kotlin.math.min
+import java.time.temporal.ChronoUnit
+import kotlin.math.roundToInt
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun TimeTable(
-    dayNames: List<String>,
-    timeTableData: TimeTableData,
-    onCellClick: (ScheduleEntity) -> Unit,
+    columnCount: Int,
+    dayHeader: @Composable (Int) -> Unit,
+    hoursLabel: @Composable () -> Unit,
+    cellCountList: List<Int>,
+    timeCell: @Composable TimeTableColumnScope.(column: Int, index: Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val startHour = min(timeTableData.earliestStartHour, tableStartHour)
-    val endHour = max(timeTableData.latestEndHour, tableEndHour)
-    val hours = (startHour..endHour).toList()
-
-    val cellCountList = timeTableData.entitiesCountList
-
-    require(dayNames.size == cellCountList.size) {
-        "size of dayNames should be equal to count of scheduleEntities"
+    require(columnCount == cellCountList.size) {
+        "columnCount should be equal to cellCountList's size"
     }
 
-    ScheduleTable(
-        columnCount = cellCountList.size,
-        dayHeader = {
-            DayHeader(dayNames[it])
-        },
-        hoursLabel = {
-            HoursLabel(hours)
-        },
-        cellCountList = cellCountList,
-        timeCell = { column, index ->
-            val dayData = timeTableData.scheduleDayDataList[column]
-            val scheduleEntity = dayData.scheduleEntities[index]
-
-            val beforeCellEndTime = if (index == 0) {
-                LocalDateTime.of(
-                    scheduleEntity.startTime.toLocalDate(),
-                    LocalTime.of(hours.first(), 0)
-                )
-            } else {
-                dayData.scheduleEntities[index - 1].endTime
+    val dayHeaders = @Composable {
+        repeat(columnCount) { dayHeader(it) }
+    }
+    val timeCells = @Composable {
+        repeat(columnCount) { column ->
+            repeat(cellCountList[column]) { cellIndex ->
+                TimeTableColumnScope.timeCell(column, cellIndex)
             }
+        }
+    }
 
-            TimeTableCell(
-                cellData = scheduleEntity,
-                onCellClick = onCellClick,
-                modifier = Modifier
-                    .timeTableCell(
-                        beforeCellEndTime,
-                        scheduleEntity.startTime,
-                        scheduleEntity.endTime,
-                        hours.size
-                    )
+    Layout(
+        contents = listOf(dayHeaders, hoursLabel, timeCells),
+        modifier = modifier,
+    ) { (dayHeaderMeasurables, hoursLabelMeasurables, timeCellMeasurables),
+        constraints ->
+
+        require(hoursLabelMeasurables.size == 1) {
+            "hoursLabel should emit only one composable"
+        }
+
+        val hoursLabelPlaceable = hoursLabelMeasurables.first().measure(constraints)
+        val dayHeaderPlaceables = dayHeaderMeasurables.map {
+            it.measure(constraints)
+        }
+        val timeCellPlaceables = timeCellMeasurables.map { measurable ->
+            val cellParentData = measurable.parentData as TimeTableParentData
+            val cellHeight = (cellParentData.duration * hoursLabelPlaceable.height).roundToInt()
+
+            measurable.measure(
+                constraints.copy(
+                    minHeight = cellHeight,
+                    maxHeight = cellHeight
+                )
             )
-        },
-        modifier = modifier
-    )
-}
+        }
 
-@Composable
-fun DayHeader(dayName: String) {
-    Text(
-        text = dayName,
-        style = MaterialTheme.typography.subtitle1, // TODO: TextStyle
-        modifier = Modifier
-            .width(tableColumnWidth)
-            .padding(vertical = 4.dp),
-        textAlign = TextAlign.Center
-    )
-}
+        val headerWidth = dayHeaderPlaceables.first().width
+        val headerHeight = dayHeaderPlaceables.first().height
+        val totalWidth = hoursLabelPlaceable.width + headerWidth * columnCount
+        val totalHeight = hoursLabelPlaceable.height + headerHeight
 
-@Composable
-fun HoursLabel(hours: List<Int>) {
-    Column {
-        hours.forEach {
-            Text(
-                text = "$it",
-                style = MaterialTheme.typography.caption,
-                modifier = Modifier
-                    .width(40.dp)
-                    .height(60.dp)
-                    .padding(4.dp),
-                textAlign = TextAlign.End
-            )
+        layout(width = totalWidth, height = totalHeight) {
+            val yPosition = headerHeight
+            var xPosition = hoursLabelPlaceable.width
+            var cellIndex = 0
+
+            hoursLabelPlaceable.placeRelative(0, yPosition)
+
+            dayHeaderPlaceables.forEachIndexed { index, headerPlaceable ->
+                headerPlaceable.placeRelative(xPosition, 0)
+
+                var yOffset = 0
+                val cellCount = cellCountList[index]
+                repeat(cellCount) {
+                    val cellPlaceable = timeCellPlaceables[cellIndex++]
+                    val cellParentData = cellPlaceable.parentData as TimeTableParentData
+                    val offset = (cellParentData.offset * hoursLabelPlaceable.height).roundToInt()
+                    yOffset += offset
+
+                    cellPlaceable.placeRelative(xPosition, yPosition + yOffset)
+
+                    yOffset += cellPlaceable.height
+                }
+
+                xPosition += headerPlaceable.width
+            }
         }
     }
 }
 
-@Preview
-@Composable
-fun HoursLabelPreview() {
-    HoursLabel(hours = (9..16).toList())
-}
+object TimeTableColumnScope {
+    @Stable
+    fun Modifier.timeTableCell(
+        beforeCellEndTime: LocalDateTime,
+        start: LocalDateTime,
+        end: LocalDateTime,
+        hoursLabelSize: Int,
+    ): Modifier {
+        val durationInHours = ChronoUnit.MINUTES.between(start, end) / 60f
+        val offset = ChronoUnit.MINUTES.between(beforeCellEndTime, start) / 60f
 
-@Preview
-@Composable
-fun TimeTablePreview() {
-    val dayNames = listOf("월", "화", "수", "목", "금")
-    TimeTable(
-        dayNames = dayNames,
-        timeTableData = timeTableData,
-        onCellClick = {}
-    )
+        return then(
+            TimeTableParentData(
+                duration = durationInHours / hoursLabelSize,
+                offset = offset / hoursLabelSize
+            )
+        )
+    }
 }
