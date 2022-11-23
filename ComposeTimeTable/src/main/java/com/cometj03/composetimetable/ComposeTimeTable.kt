cf584130550c158f82b4af684216d4271838e72c
cometj03 <harryjung1001@gmail.com>
2022-11-23 16:14:12 +0900

    Delete app directory

--- ComposeTimeTable/src/main/java/com/cometj03/composetimetable/ComposeTimeTable.kt
+++ ComposeTimeTable/src/main/java/com/cometj03/composetimetable/ComposeTimeTable.kt
@@ -1,6 +1,5 @@
 package com.cometj03.composetimetable
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
